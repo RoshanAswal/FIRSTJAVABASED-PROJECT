665ce8a3249fe91399d72804975ef12deb136a18
RoshanAswal <aswalroshan2002@gmail.com>
2022-03-31 20:29:54 +0530

    second commit

---
+++ Unit_01/P3_Task01_Variable.java
@@ -0,0 +1,16 @@
+package Unit_01;
+
+public class P3_Task01_Variable {
+
+	public static void main(String[] args) {
+		String name="Marcus";
+		int age=25;
+		long salary=500000;
+		float tax=20.255f;
+		System.out.println("Name : "+name);
+		System.out.println("Age : "+age);
+		System.out.println("Salary : "+salary);
+		System.out.println("Tax : "+tax);
+	}
+
+}
---
+++ Unit_01/P3_Task02_TypeConversion.java
@@ -0,0 +1,13 @@
+package Unit_01;
+
+public class P3_Task02_TypeConversion {
+
+	public static void main(String[] args) {
+		int num=99;
+		double n=num;
+		System.out.println((char)num);
+		System.out.println(n);
+
+	}
+
+}
